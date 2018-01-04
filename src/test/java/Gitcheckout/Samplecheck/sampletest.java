07277252402b8843ad0ad5dcdf53ab39f9f0266f
suresh <jureddi@gmail.com>
2018-01-04 13:19:16 -0600

    adding testng and modified testcase files

--- src/test/java/Gitcheckout/Samplecheck/sampletest.java
+++ src/test/java/Gitcheckout/Samplecheck/sampletest.java
@@ -1,17 +1,23 @@
 package Gitcheckout.Samplecheck;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 
 public class sampletest {
-
-	public static void main(String[] args) {
+	
+	@Test
+	public void jenkins() {
+	
+	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Drivers\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
 	driver.manage().window().maximize();
-	driver.navigate().to("https://www.google.com/");
+	driver.get("http://www.google.com");
 	driver.findElement(By.id("lst-ib")).sendKeys("Abdulkalam");
-	
+	driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
 	}
 
 }
